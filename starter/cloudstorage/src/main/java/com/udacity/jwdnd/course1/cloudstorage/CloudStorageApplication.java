a42752ee1457dbc9fe63085b382c68036814926c
samad <samadarshad@hotmail.co.uk>
2021-02-16 14:02:51 +0000

    temp disable security during development

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplication.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplication.java
@@ -3,7 +3,7 @@ package com.udacity.jwdnd.course1.cloudstorage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {      org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
 public class CloudStorageApplication {
 
 	public static void main(String[] args) {
