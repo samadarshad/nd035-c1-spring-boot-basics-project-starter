d06458b38b7489fa051b1aa154149b715ec46247
samad <samadarshad@hotmail.co.uk>
2021-02-17 15:40:41 +0000

    working post notes data

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/NotesController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/NotesController.java
@@ -6,14 +6,19 @@ import com.udacity.jwdnd.course1.cloudstorage.services.NoteService;
 import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/notes")
 public class NotesController {
+
+    @PostMapping
+    public String addNote(@RequestParam("noteTitle") String noteTitle,
+                          @RequestParam("noteDescription") String noteDescription
+    ) {
+        System.out.println("adding note" + noteTitle + noteDescription);
+        return "redirect:/";
+    }
 
     @DeleteMapping("/{id}")
     public String deleteNote(@PathVariable Integer id, Model model) {
