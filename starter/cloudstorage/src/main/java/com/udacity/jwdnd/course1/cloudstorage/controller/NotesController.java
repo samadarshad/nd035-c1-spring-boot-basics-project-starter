0671373043e2cfa1056f32e53c9792dbc875f1d7
samad <samadarshad@hotmail.co.uk>
2021-02-17 19:24:10 +0000

    notes uses utils too for error

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/NotesController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/NotesController.java
@@ -4,6 +4,7 @@ import com.udacity.jwdnd.course1.cloudstorage.model.Note;
 import com.udacity.jwdnd.course1.cloudstorage.model.User;
 import com.udacity.jwdnd.course1.cloudstorage.services.NoteService;
 import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
+import com.udacity.jwdnd.course1.cloudstorage.utility.Utils;
 import org.springframework.http.HttpStatus;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Controller;
@@ -17,10 +18,12 @@ public class NotesController {
 
     private UserService userService;
     private NoteService noteService;
+    private Utils utils;
 
-    public NotesController(UserService userService, NoteService noteService) {
-    this.userService=userService;
-    this.noteService=noteService;
+    public NotesController(UserService userService, NoteService noteService, Utils utils) {
+        this.userService=userService;
+        this.noteService=noteService;
+        this.utils = utils;
     }
 
     @PostMapping
@@ -37,17 +40,7 @@ public class NotesController {
         } else {
             Note note = new Note(noteId, noteTitle, noteDescription, user.getUserId());
             Note existingNote = noteService.get(note.getNoteId());
-            if (existingNote == null) {
-                throw new ResponseStatusException(
-                        HttpStatus.NOT_FOUND, "Note does not exist."
-                );
-            };
-
-            if (existingNote.getUserId() != user.getUserId()) {
-                throw new ResponseStatusException(
-                        HttpStatus.UNAUTHORIZED, "User is not authorized to access that note."
-                );
-            }
+            utils.checkItemExistsAndUserIsAuthorizedOrThrowError(existingNote, user);
             noteService.update(note);
         }
         return "redirect:/";
@@ -57,17 +50,7 @@ public class NotesController {
     public String delete(@PathVariable("id") Integer noteId, Model model) {
         Note existingNote = noteService.get(noteId);
         User user = userService.get("user1"); //get this from auth
-        if (existingNote == null) {
-            throw new ResponseStatusException(
-                    HttpStatus.NOT_FOUND, "Note does not exist."
-            );
-        };
-
-        if (existingNote.getUserId() != user.getUserId()) {
-            throw new ResponseStatusException(
-                    HttpStatus.UNAUTHORIZED, "User is not authorized to access that note."
-            );
-        }
+        utils.checkItemExistsAndUserIsAuthorizedOrThrowError(existingNote, user);
         noteService.delete(noteId);
         return "redirect:/";
     }
