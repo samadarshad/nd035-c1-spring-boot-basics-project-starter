2201eb408b1ae5a977140dd81a970c413b9479e3
samad <samadarshad@hotmail.co.uk>
2021-02-18 21:04:22 +0000

    working assert download

--- starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/FileControllerTests.java
+++ starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/FileControllerTests.java
@@ -19,7 +19,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.*;
 
 import static com.udacity.jwdnd.course1.cloudstorage.utility.Utils.addItemsToUser;
@@ -44,9 +48,11 @@ class FileControllerTests {
 
     private static final User user1 = new User(null, "user1", null, "pass1", "first1", "last1");
     private static final User user2 = new User(null, "user2", null, "pass2", "first2", "last2");
-    private static final byte [] fileData1 = "Hello World1".getBytes(StandardCharsets.UTF_8);
+    private static final byte [] fileData1a = "Hello World1a".getBytes(StandardCharsets.UTF_8);
+    private static final byte [] fileData1b = "Hello World1b".getBytes(StandardCharsets.UTF_8);
     private static final byte [] fileData2 = "Hello World2".getBytes(StandardCharsets.UTF_8);
-    private static final File file1 = new File(null, "fileName1", "application/octet-stream", (long) fileData1.length, null, fileData1);
+    private static final File file1a = new File(null, "fileName1a", "application/octet-stream", (long) fileData1a.length, null, fileData1a);
+    private static final File file1b = new File(null, "fileName1b", "application/octet-stream", (long) fileData1b.length, null, fileData1b);
     private static final File file2 = new File(null, "fileName2", "application/octet-stream", (long) fileData2.length, null, fileData2);
 
     private static final String downloadsDirectory = System.getProperty("user.dir") + java.io.File.separator + "testDownloads";
@@ -63,7 +69,7 @@ class FileControllerTests {
         WebDriverManager.chromedriver().setup();
 
         userService.createAndUpdateObject(user1);
-        addItemsToUser(user1, crudServices, file1);
+        addItemsToUser(user1, crudServices, file1a, file1b);
 
         userService.createAndUpdateObject(user2);
         addItemsToUser(user2, crudServices, file2);
@@ -96,31 +102,51 @@ class FileControllerTests {
     }
 
     @Test
-    void user1CanDownloadTheirFile() throws InterruptedException {
+    void user1CanReadFilenames() {
         Utils.login(driver, port, "user1", "pass1");
         HomePage homePage = new HomePage(driver);
         homePage.navFilesTab.click();
         homePage.waitForFiles(driver);
         List<String> fileNames = homePage.getFileNameList();
-        assert(fileNames.size() == 1);
-        int index = 0;
-        homePage.downloadFile(index);
-        Thread.sleep(downloadWaitTime); // wait for download
-        java.io.File downloadedFile = new java.io.File(downloadsDirectory + java.io.File.separator + fileNames.get(index));
 
-
-        assertTrue(downloadedFile.exists());
+        List<String> expectedFileNames = Arrays.asList("fileName1");
+        assertEquals(fileNames, expectedFileNames);
     }
 
     @Test
-    void user1CanDownloadTheirFileViaUrl() throws InterruptedException {
+    void user1CanDownloadTheirFiles() throws InterruptedException, IOException {
         Utils.login(driver, port, "user1", "pass1");
-        driver.get("http://localhost:" + port + "/files/" + file1.getFileId());
+        HomePage homePage = new HomePage(driver);
+        homePage.navFilesTab.click();
+        homePage.waitForFiles(driver);
+        List<String> fileNames = homePage.getFileNameList();
+        int i = 0;
+
+        homePage.downloadFile(i);
         Thread.sleep(downloadWaitTime); // wait for download
-        java.io.File downloadedFile = new java.io.File(downloadsDirectory + java.io.File.separator + file1.getFileName());
 
-
+        java.io.File downloadedFile = new java.io.File(downloadsDirectory + java.io.File.separator + fileNames.get(i));
         assertTrue(downloadedFile.exists());
+
+        byte[] fileContent = Files.readAllBytes(Path.of(downloadsDirectory + java.io.File.separator + fileNames.get(i)));
+        assertArrayEquals(fileContent, file1a.getFileData());
+
+    }
+
+    @Test
+    void user1CanDownloadTheirFilesViaUrl() throws InterruptedException {
+        Utils.login(driver, port, "user1", "pass1");
+        driver.get("http://localhost:" + port + "/files/" + file1a.getFileId());
+        Thread.sleep(downloadWaitTime); // wait for download
+        java.io.File downloadedFile1a = new java.io.File(downloadsDirectory + java.io.File.separator + file1a.getFileName());
+
+        assertTrue(downloadedFile1a.exists());
+
+        driver.get("http://localhost:" + port + "/files/" + file1b.getFileId());
+        Thread.sleep(downloadWaitTime); // wait for download
+        java.io.File downloadedFile1b = new java.io.File(downloadsDirectory + java.io.File.separator + file1b.getFileName());
+
+        assertTrue(downloadedFile1b.exists());
     }
 
     @Test
