2c84c10dd0954c8535b506a96d32cdb0e005a97b
samad <samadarshad@hotmail.co.uk>
2021-02-18 07:08:38 +0000

    enabling security

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplication.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplication.java
@@ -3,7 +3,7 @@ package com.udacity.jwdnd.course1.cloudstorage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = {      org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class CloudStorageApplication {
 
 	public static void main(String[] args) {
