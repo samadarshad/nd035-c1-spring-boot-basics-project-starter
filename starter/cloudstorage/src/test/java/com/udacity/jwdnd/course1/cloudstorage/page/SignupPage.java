c81d234a7b376d5ad7635574f4150d387713f4dd
samad <samadarshad@hotmail.co.uk>
2021-02-20 14:23:38 +0000

    new user signup

--- starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/page/SignupPage.java
+++ starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/page/SignupPage.java
@@ -4,6 +4,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static com.udacity.jwdnd.course1.cloudstorage.page.Utils.WebDriverWaitTimeoutSeconds;
 
 public class SignupPage {
     @FindBy(id = "inputFirstName")
@@ -21,8 +25,19 @@ public class SignupPage {
     @FindBy(id = "submit-button")
     private WebElement submitButton;
 
+    @FindBy(id = "success")
+    private WebElement successMsg;
+
     public SignupPage (WebDriver driver) {
         PageFactory.initElements(driver, this);
+    }
+
+    public void waitForPage(WebDriver driver) {
+        new WebDriverWait(driver, WebDriverWaitTimeoutSeconds).until(ExpectedConditions.elementToBeClickable(submitButton));
+    }
+
+    public void waitForSuccess(WebDriver driver) {
+        new WebDriverWait(driver, WebDriverWaitTimeoutSeconds).until(ExpectedConditions.visibilityOf(successMsg));
     }
 
     public void signup(String firstname, String lastname, String username, String password) {
